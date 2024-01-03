de5c1bc6dbcb315b0139c8215913886dcb0ccb0a
Plumpsy1 <155482986+Plumpsy1@users.noreply.github.com>
2024-01-03 12:05:33 +0000

    Update scan.js

--- scan.js
+++ scan.js
@@ -19,13 +19,42 @@ export function list_servers(ns) {
 /** @param {NS} ns **/
 export async function main(ns) {
   while(true){
+    let file = 0;
+
+    if(ns.fileExists("BruteSSH.exe")){
+      file = file++
+    }
+
+    if(ns.fileExists("FTPCrack.exe")){
+      file = file++
+    }
+
+    if(ns.fileExists("relaySMTP.exe")){
+      file = file++
+    }
+
+
+    if(ns.fileExists("HTTPWorm.exe")){
+      file = file++
+    }
+
+    if(ns.fileExists("SQLInject.exe")){
+      file = file++
+    }
+
 	  const servers = list_servers(ns).filter(s => !ns.hasRootAccess(s));
     let sLen = servers.length;
       for (let i = 0; i < sLen; i++){
         let maxLevel = ns.getHackingLevel();
         let serverLevel = ns.getServerRequiredHackingLevel(servers[i]);
+        let ports = ns.getServerNumPortsRequired(servers[i]);
         if(serverLevel <= maxLevel) {
-          ns.tprint(`${servers[i]} is ready to hack`)
+          if (ports <= file){
+            ns.tprint(`${servers[i]} is ready to hack`)
+          }
+          else{
+            ns.tprint("Your require a new program")
+          }  
         }
       }
     await(ns.sleep(60000))    
