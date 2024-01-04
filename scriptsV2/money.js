77b568ff8909045471ad9a877619bb3e860df000
Plumpsy1 <boobbop274@gmail.com>
2024-01-04 19:05:02 +0000

    Update money.js

--- scriptsV2/money.js
+++ scriptsV2/money.js
@@ -1,34 +1,39 @@
 export async function main(ns) {
-    const host = ns.getHostname();
-    const hackTime = ns.getHackTime(host);
-    const growTime = ns.getGrowTime(host);
-    const weakenTime = ns.getWeakenTime(host);
+  const host = ns.getHostname();
+  const hackTime = ns.getHackTime(host);
+  const growTime = ns.getGrowTime(host);
+  const weakenTime = ns.getWeakenTime(host);
 
-    while (true) {
+  while (true) {
 
-      if (ns.getServerSecurityLevel(host) > ns.getServerMinSecurityLevel(host)) {
-        const script = "scriptsV2/weaken.js";
-        const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
+    if (ns.getServerSecurityLevel(host) > ns.getServerMinSecurityLevel(host)) {
+      const script = "scriptsV2/weaken.js";
+      const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
+      if (threads > 0){
         await ns.exec(script,host,threads,host);
         await ns.sleep(weakenTime)
       }
+    }
   
-      else if (ns.getServerMoneyAvailable(host) < ns.getServerMaxMoney(host)) {
-        const script = "scriptsV2/grow.js";
-        const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
+    else if (ns.getServerMoneyAvailable(host) < ns.getServerMaxMoney(host)) {
+      const script = "scriptsV2/grow.js";
+      const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
+      if (threads > 0){
         await ns.exec(script,host,threads,host);
-        await ns.sleep(growTime);
+        await ns.sleep(growTime)
       }
+    }
   
-      else {
-        const script = "scriptsV2/hack.js";
-        const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
+    else {
+      const script = "scriptsV2/hack.js";
+      const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
+      if (threads > 0){
         await ns.exec(script,host,threads,host);
-        await ns.sleep(hackTime);
+        await ns.sleep(hackTime)
       }
-
     }
 
+  }
 }
   
   
