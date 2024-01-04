ce7306454e515df80b39e20f68e532c9c42ccb20
Plumpsy1 <boobbop274@gmail.com>
2024-01-04 20:41:29 +0000

    Update save.js

--- scriptsV2/save.js
+++ scriptsV2/save.js
@@ -1,6 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    let amount = ns.args[0];
+    const amount = ns.args[0];
+
     ns.scriptKill("scriptsV2/node.js","home")
     ns.run("scriptsV2/node.js",1,amount)
 }
